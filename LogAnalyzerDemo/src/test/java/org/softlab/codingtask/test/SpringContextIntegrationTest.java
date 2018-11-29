a10f7dc9b082dba5bf812d86d1016cd5cc72fb1c
Pawel Wloch <pawel_wloch@poczta.onet.pl>
2018-11-29 15:19:11 +0100

    removing dependency of junit on test file location

--- LogAnalyzerDemo/src/test/java/org/softlab/codingtask/test/SpringContextIntegrationTest.java
+++ LogAnalyzerDemo/src/test/java/org/softlab/codingtask/test/SpringContextIntegrationTest.java
@@ -5,14 +5,24 @@
 package org.softlab.codingtask.test;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.softlab.codingtask.Application;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 public class SpringContextIntegrationTest {
+
+	@Autowired
+	private ResourceLoader resourceLoader;
+
+	private final static String JSON_TEST_FILE = "classpath:test_data.json";
 
 	@Test
 	public final void testMain() throws Exception {
-		// put here a path to json file in ./test/resources
-		Application.main(new String[] { "d:\\git_repos\\LogAnalyzerDemo\\src\\test\\resources\\test_data.json" });
+		String path2JsonTestFile = resourceLoader.getResource(JSON_TEST_FILE).getFile().getPath();
+		Application.main(new String[] { path2JsonTestFile });
 	}
 
 }
